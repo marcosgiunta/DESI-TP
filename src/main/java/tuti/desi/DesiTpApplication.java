44c240c596e422853b9c29f8262f1d45f9756bc9
marco <marco@Marcos>
2025-06-18 12:08:25 -0300

    subid

---
+++ src/main/java/tuti/desi/DesiTpApplication.java
@@ -0,0 +1,13 @@
+package tuti.desi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesiTpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesiTpApplication.class, args);
+	}
+
+}
