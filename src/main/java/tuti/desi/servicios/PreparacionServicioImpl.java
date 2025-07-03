6d50b0bb64fed535f4f7eda5cda2c52ac4b365a3
marcos <marcosgiunta1@gmail.com>
2025-07-02 22:57:29 -0300

    modificaciones a los html

--- src/main/java/tuti/desi/servicios/PreparacionServicioImpl.java
+++ src/main/java/tuti/desi/servicios/PreparacionServicioImpl.java
@@ -18,39 +18,32 @@ public class PreparacionServicioImpl implements PreparacionServicio {
     @Override
     public Preparacion guardar(Preparacion preparacion) {
 
- if (preparacion.getFechaPreparacion().after(new Date())) {
+        if (preparacion.getFechaPreparacion().after(new Date())) {
             throw new IllegalArgumentException("La fecha no puede ser futura.");
         }
 
-List<Preparacion> existentes = preparacionRepositorio.buscarPorFiltros(
-            preparacion.getFechaPreparacion(), 
-            preparacion.getReceta().getNombre()
-        );
+        List<Preparacion> existentes = preparacionRepositorio.buscarPorFiltros(
+                preparacion.getFechaPreparacion(),
+                preparacion.getReceta().getNombre());
         for (Preparacion p : existentes) {
             if (p.getReceta().getId().equals(preparacion.getReceta().getId())) {
                 throw new IllegalArgumentException("Ya existe una preparación de esta receta para la fecha indicada.");
             }
         }
 
+        // if (!hayStockSuficiente(preparacion)) {
+        // throw new IllegalArgumentException("No hay stock suficiente para preparar la
+        // cantidad de raciones indicadas.");
+        // }
 
- //if (!hayStockSuficiente(preparacion)) {
-           // throw new IllegalArgumentException("No hay stock suficiente para preparar la cantidad de raciones indicadas.");
-      //  }
-
-
-            //private boolean hayStockSuficiente(Preparacion preparacion) {
+        // private boolean hayStockSuficiente(Preparacion preparacion) {
         // Aquí deberías recorrer los ingredientes de la receta y chequear el stock
         // Retornar true si hay stock suficiente, false si no
-       // return true; // Implementa la lógica real aquí
-    //}
-
+        // return true; // Implementa la lógica real aquí
+        // }
 
         return preparacionRepositorio.save(preparacion);
 
-
-
-
-        
     }
 
     @Override
